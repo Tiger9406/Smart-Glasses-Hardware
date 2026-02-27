33ac932674adc3591bebe49893be2ef2a90fe8b8
Tiger <xcao2005@gmail.com>
2026-02-26 20:02:30 -0500

    define frame types, structure of frame & audio buffer

--- include/app_httpd.h
+++ include/app_httpd.h
@@ -3,6 +3,23 @@
 
 #include "esp_http_server.h"
 
+// Frame types
+#define WS_FRAME_VIDEO 0x01
+#define WS_FRAME_AUDIO_RX 0x02
+
+typedef struct {
+    uint8_t *data;
+    size_t len;
+} video_frame_t;
+
+typedef struct {
+    int16_t samples[512];
+    size_t count;
+} audio_buffer_t;
+
+extern QueueHandle_t video_queue;
+extern QueueHandle_t audio_tx_queue;
+
 // Function declarations
 void startServer();
 void setupLedFlash();
